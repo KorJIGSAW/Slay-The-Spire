6f94f69e02afbf2e5299c3d29cb9362b92c61ef3
KorJIGSAW <111522731+KorJIGSAW@users.noreply.github.com>
2023-05-17 05:18:28 +0900

    add card description from card.txt

--- card.h
+++ card.h
@@ -26,7 +26,33 @@ typedef struct {
 } CardType;
 
 typedef struct {
-	CardType* const cardtype;
+    CardType* cardtype;
 } Card;
+
+
+void ReadCardData(const char* filename, struct Card* card, int numCards) { //txt파일로부터 카드의 정보를 읽어온다.
+    FILE* file = fopen(filename, "r"); //읽기로
+    if (file == NULL) {
+        printf("파일을 열 수 없습니다.\n");
+        return;
+    }
+
+    for (int i = 0; i < numCards; i++) {
+        //fscanf(file, );
+        //   fscanf(file, "%d %s %d %d", &cards[i].id, cards[i].name, &cards[i].attack, &cards[i].defense); 형식
+        // ... 필요한 정보를 읽어와서 구조체에 저장합니다.
+    }
+
+    fclose(file);
+}
+
+
+void SetCard(GameManager *gm) {
+    Card *card;  // 최대 20개의 카드를 저장할 수 있는 배열
+    card = (Card*)malloc(sizeof(CardType*)*20);  
+    // "cards.txt" 파일로부터 카드 정보를 읽어옵니다.
+    ReadCardData("card.txt", card, 100);
+
+}
 
 #endif // !STATUS_H
