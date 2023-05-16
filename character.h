3528ca873a02f3e4ae5efcd4fca81227ed5c2886
Pumpkinjam <rnjsl1212@naver.com>
2023-05-17 04:50:40 +0900

    fixed error

--- character.h
+++ character.h
@@ -1,51 +1,25 @@
 #ifndef CHARACTER_H
 #define CHARACTER_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include "gamemanager.h"
+typedef struct MyCharacter {
+	int hp; //내 체력
+	int power; //힘을 얻었을 때
+	int shield; //내 방어도
+}MyCharacter;
 
-typedef struct {
+typedef struct EnemyCharacter {
+	int hp;
 	int power;
-	int vulnerable;
-	int weakness;
-	
-}Buff;
+	int shield;
+}EnemyCharacter;
 
+/* 대안
 typedef struct Character {
 	int hp;
+	int power;
 	int shield;
 	int isPlayer;	//0 or 1
-
-	Buff buff;
-	
 }Character;
-
-void StartBattle() { // 전투 시작함수
-
-}
-
-void CheckBattleEnd(GameManager* gm) { //적이 체력이 0이 되었을때, 플레이어가 체력이 0이 되었을때
-
-	short is_cleared = 1;
-
-	if (gm->player->hp <= 0) {
-		// 게임 엔딩 후 점수 인출 및 첫 화면 복귀
-	}
-	else {
-		// 다음층 진출 or 게임 엔딩
-		for (int i = 0, z = gm->enemy_count; i < z; i++) {
-			if (gm->enemy[i]->hp > 0) {
-				is_cleared = 0;
-				break;
-			}
-		}
-	}
-
-	if (is_cleared) {
-		// todo: goto next level
-	}
-}
 
 void do_damage(Character* attacker, Character* defender, int damage) {
 	if (defender->shield < damage) {
@@ -62,5 +36,6 @@ int attack(Character* attacker, Character* defender, int damage, int hits) {
 		do_damage(attacker, defender, damage+attacker->power);
 	}
 }
+*/
 
 #endif // !CHARACTER_H
