d9080abfee63d86a3ce5c9320e0d719c5fad946d
Pumpkinjam <rnjsl1212@naver.com>
2023-05-31 21:46:12 +0900

    enumerated card type

--- card.h
+++ card.h
@@ -16,6 +16,28 @@ typedef struct Card {
 	char description[150];
 }Card;
 
+/* Kind and nice translations
+ * [ 0] STRIKE : 타격
+ * [ 1] DEFEND : 수비
+ * [ 2] BASH : 강타
+ * [ 3] PERFECTED_STRIKE : 완벽한 타격
+ * [ 4] INFLAME : 발화
+ * [ 5] IRON_WAVE : 철의 파동
+ * [ 6] BLOODLETTING : 사혈
+ * [ 7] PUMMEL : 연타
+ * [ 8] IMMOLATE : 화형
+ * [ 9] OFFERING : 제물
+ * [10] IMPREVIOUS : 무적
+ * [11] DEMON_FORM : 악마의 형상
+ * [12] BLUDGEON : 몽둥이질
+ * [13] DROPKICK : 드롭킥
+ */
+typedef enum {
+	STRIKE, DEFEND, BASH, PERFECTED_STRIKE, INFLAME,
+	IRON_WAVE, BLOODLETTING, PUMMEL, IMMOLATE, OFFERING,
+	IMPREVIOUS, DEMON_FORM, BLUDGEON, DROPKICK
+} CardType;
+
 Card card[14];
 
 ListNode* Init_Card() { //초기 카드 세팅
@@ -31,8 +53,8 @@ ListNode* Init_Card() { //초기 카드 세팅
 	return head;
 }
 
-void AddCardToDeck() { //선택한 카드를 덱에 추가하는 함수
-
+void AddCardToDeck(CardType card, ListNode* MyDeck) { //선택한 카드를 덱에 추가하는 함수
+	
 }
 
 void MixCard(int CardCount, ListNode* MyDeck) {//	덱에서 카드를 섞는다.
